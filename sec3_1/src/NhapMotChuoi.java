112597e238265fbf43d85190593936abe74e4385
tandat01 <dat01101999@gmail.com>
2019-04-20 16:50:08 +0700

    sec3

---
+++ sec3_1/src/NhapMotChuoi.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class NhapMotChuoi {
+    public static void main(String args[]){
+        String n;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhap mot chuoi");
+        n=scanner.nextLine();
+        System.out.println("Chuoi vua nhap la:"+n);
+    }
+    }
