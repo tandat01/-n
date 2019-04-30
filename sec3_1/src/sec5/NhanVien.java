adf1211e5a06638d1114d339409ebb6b85ebe487
tandat01 <dat01101999@gmail.com>
2019-04-30 09:19:20 +0700

    sec4

---
+++ sec3_1/src/sec5/NhanVien.java
@@ -0,0 +1,91 @@
+package sec5;
+
+import java.util.Scanner;
+
+public class NhanVien {
+    public String Ten;
+    public int Tuoi;
+    public String Diachi;
+    public double TienLuong;
+    public int Tonggiolam;
+
+    public NhanVien() {
+    }
+
+    public NhanVien(String ten, int tuoi, String diachi, double tienLuong, int tonggiolam) {
+        Ten = ten;
+        Tuoi = tuoi;
+        Diachi = diachi;
+        TienLuong = tienLuong;
+        Tonggiolam = tonggiolam;
+    }
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String ten) {
+        Ten = ten;
+    }
+
+    public int getTuoi() {
+        return Tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        Tuoi = tuoi;
+    }
+
+    public String getDiachi() {
+        return Diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        Diachi = diachi;
+    }
+
+    public double getTienLuong() {
+        return TienLuong;
+    }
+
+    public void setTienLuong(double tienLuong) {
+        TienLuong = tienLuong;
+    }
+
+    public int getTonggiolam() {
+        return Tonggiolam;
+    }
+
+    public void setTonggiolam(int tonggiolam) {
+        Tonggiolam = tonggiolam;
+    }
+    public void inputInfo(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhap ten: ");
+        Ten=scanner.nextLine();
+        System.out.println("Nhap tuoi: ");
+        Tuoi=scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Dia chi : ");
+        Diachi=scanner.nextLine();
+        System.out.println("Tien luong la: ");
+        TienLuong=scanner.nextByte();
+        System.out.println("Tong gio lam: ");
+        Tonggiolam=scanner.nextByte();
+    }
+    public void printInfo(){
+        System.out.println("Ten la: "+Ten);
+        System.out.println("Tuoi la: "+Tuoi);
+        System.out.println("Dia chi la: "+Diachi);
+        System.out.println("Tien la: "+TienLuong);
+        System.out.println("Tong gio lam la: "+Tonggiolam);
+    }
+    public void tinhThuong(){
+        if (Tonggiolam>=200){
+            TienLuong=TienLuong+(TienLuong*0.2);
+        }else if (100<=Tonggiolam&&Tonggiolam<200){
+            TienLuong=TienLuong+(TienLuong*0.1);
+        }else
+            System.out.println("Tong tien luong va thuong la: "+TienLuong);
+    }
+}
